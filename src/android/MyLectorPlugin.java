5890d7732a4f972a5cdd4071b43fd03019255ca5
Michael <michgomo08.2@gmail.com>
2020-07-01 16:30:09 -0600

    Se cambia

--- src/android/MyLectorPlugin.java
+++ src/android/MyLectorPlugin.java
@@ -1,56 +1,43 @@
 package com.mylectorplugin;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
+import org.apache.cordova.*;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
+import android.content.*;
 import android.os.*;
-import com.ftsafe.PcscServer;
 import com.ftsafe.readerScheme.FTReader;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class MyLectorPlugin extends CordovaPlugin {
+
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+    }
+
     String message ="";
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
+        
 
+            Context context = cordova.getActivity().getApplicationContext();
+            if (action.equals("coolMethod")) {
+                message = "Hola mundo de Costa Rica a Nicaragua 3 :)";
+                this.openNewActivity(context);
+                return true;
+            }
+            return false;
 
-            message = "Hola mundo de Costa Rica a Nicaragua 3 :)";
+    }
 
-            final Handler mHandler = new Handler(){
-                @Override
-                public void handleMessage(Message msg){
-                    super.handleMessage(msg);
-                    message += "---- entro al handler ---";
-                    switch (msg.what) {
-                        case 0:
-                        String log = msg.obj.toString();
-                        message +="LOG---------->"+log;
-                        break;
-                    }
-                }
-            };
-
-            mHandler.sendMessage(mHandler.obtainMessage(0, "Prueba"));
-
-            int PORT = 0x096e; 
-            PcscServer pcscServer = new PcscServer(PORT,Activity.getApplicationContext(), mHandler);
-           /*
-            FTReader  ftReader = pcscServer.getFtReaderObject();
-            new Tpcsc().testA(PORT); 
-            */
-
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
-        return false;
+    private void openNewActivity(Context context) {
+        Intent intent = new Intent(context, NewActivity.class);
+        this.cordova.getActivity().startActivity(intent);
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
