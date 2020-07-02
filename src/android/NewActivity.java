70d43611b95de36ae49007ccf67836106c60674a
Michael <michgomo08.2@gmail.com>
2020-07-02 11:59:32 -0600

    Se cambia

--- src/android/NewActivity.java
+++ src/android/NewActivity.java
@@ -1,7 +1,9 @@
 package com.mylectorplugin;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.app.*;
+import android.os.*;
+import android.widget.*;
+import android.view.*;
 
 public class NewActivity extends Activity {
 
@@ -10,5 +12,34 @@ public class NewActivity extends Activity {
         super.onCreate(savedInstanceState);
         String package_name = getApplication().getPackageName();
         setContentView(getApplication().getResources().getIdentifier("activity_new", "layout", package_name));
+
+        findViewById(getResources().getIdentifier("button_conectar", "id", getPackageName())).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                mHandler.sendMessage(mHandler.obtainMessage(-1, ""));
+            }
+        });
     }
+
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg){
+            super.handleMessage(msg);
+            TextView textView = (TextView) findViewById(getResources().getIdentifier("textView", "id", getPackageName()));
+
+            String log=null;
+
+            switch (msg.what) {
+                case -1:
+                    textView.setText("AAAAAAAAAAAAAAAAAAAAAAAAAA-08");
+                    return;
+                }
+            }
+        };
+        
+        private void showLog(String log){
+            mHandler.sendMessage(mHandler.obtainMessage(0, log));
+        }
 }
+
+
